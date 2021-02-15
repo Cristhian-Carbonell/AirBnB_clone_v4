6920fd10e3d6ee3c0f29ac0f0c07654f7b741749
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-02-15 18:58:47 -0500

    Update file 1.hbnb.py, 1-hbnb.html and 1-hbnb.js

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,7 +1,19 @@
-let idAmenity = []
-document.addEventListener('DOMContentloaded', function (event) {
-  if ($('checkbox').prop('checkbox')) {
-    idAmenity.push($('#amenity_id'));
-  }
-})
-
+/*
+*/
+$(document).ready(function () {
+  const idAmenities = {};
+  $('input:checkbox').click(function () {
+    $(this).each(function () {
+      if (this.checked) {
+        idAmenities[$(this).data('id')] = $(this).data('name');   
+      } else {
+        delete idAmenities[$(this).data('id')];
+      }
+    });
+    if (Object.values(idAmenities).length > 0) {
+      $('.amenities h4').text(Object.values(idAmenities).join(', '));
+    } else {
+      $('.amenities h4').html('&nbsp');
+    }
+  });
+});
